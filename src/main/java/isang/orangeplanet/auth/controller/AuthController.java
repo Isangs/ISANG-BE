0e5c2acbcb50cad81b2839d4e97045aa5ec84656
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-08-02 15:57:38 +0900

    카카오 로그인 화면 호출 서비스 구현 (#6)

--- src/main/java/isang/orangeplanet/auth/controller/AuthController.java
+++ src/main/java/isang/orangeplanet/auth/controller/AuthController.java
@@ -1,10 +1,19 @@
 package isang.orangeplanet.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import isang.orangeplanet.auth.service.AuthService;
+import isang.orangeplanet.global.api_response.ApiResponse;
+import isang.orangeplanet.global.api_response.exception.GeneralException;
+import isang.orangeplanet.global.api_response.status.ErrorStatus;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -13,5 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
   private final AuthService authService;
 
+  @GetMapping(value = "/oauth2/kakao")
+  @Operation(summary = "카카오 로그인 화면 호출", description = "카카오 로그인 화면 호출 엔드포인트")
+  public ApiResponse<Void> kakaoLogin(@NonNull HttpServletResponse response) {
+    try {
+      response.sendRedirect(authService.kakaoLoginUrl());
+    } catch (IOException e) {
+      throw new GeneralException(ErrorStatus.INTERNAL_ERROR, "카카오 로그인 화면 요청중 문제가 발생했습니다.");
+    }
 
+    return ApiResponse.onSuccess();
+  }
 }
--- src/main/java/isang/orangeplanet/auth/service/AuthService.java
+++ src/main/java/isang/orangeplanet/auth/service/AuthService.java
@@ -1,11 +1,23 @@
 package isang.orangeplanet.auth.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
+  @Value("${oauth2.client-id}")
+  private String clientId;
 
+  @Value("${oauth2.client-secret}")
+  private String clientSecretKey;
+
+  @Value("${oauth2.redirect-uri}")
+  private String redirectUri;
+
+  public String kakaoLoginUrl() {
+    return "https://kauth.kakao.com/oauth/authorize?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&response_type=code";
+  }
 }
