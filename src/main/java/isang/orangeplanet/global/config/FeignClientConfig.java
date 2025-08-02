0f53739be5bb25b82aabc15f959488404e7b10b5
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-08-02 16:00:53 +0900

    Feign 의존성 추가 및 Config 파일 설정 (#6)

---
+++ src/main/java/isang/orangeplanet/global/config/FeignClientConfig.java
@@ -0,0 +1,30 @@
+package isang.orangeplanet.global.config;
+
+import feign.Request;
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * FeignClientConfig : 외부 API 호출 설정 파일
+ */
+@Configuration
+public class FeignClientConfig {
+
+  @Bean
+  Request.Options requestOptions() {
+    return new Request.Options(15, TimeUnit.SECONDS, 15, TimeUnit.SECONDS, false);
+  }
+
+  @Bean
+  Retryer retry() {
+    return new Retryer.Default(500, 1000, 2);
+  }
+
+  @Bean
+  System.Logger.Level feignLoggerLevel() {
+    return System.Logger.Level.ALL;
+  }
+}
