aced5c9c4c0f682c5366cece08daebb40120443e
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-08-03 12:06:29 +0900

    회원 조회 응답 객체 구현 (#15)

--- src/main/java/isang/orangeplanet/domain/user/controller/UserController.java
+++ src/main/java/isang/orangeplanet/domain/user/controller/UserController.java
@@ -2,7 +2,9 @@ package isang.orangeplanet.domain.user.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import isang.orangeplanet.domain.user.service.UserService;
+import isang.orangeplanet.global.api_response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
   private final UserService userService;
 
-
+  @GetMapping(value = "/detail")
+  public ApiResponse<Void> detailUser() {
+    return ApiResponse.onSuccess();
+  }
 }
