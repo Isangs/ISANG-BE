1f1f59e35323964e6faa77f851b19e038a43c9f6
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-08-03 11:14:04 +0900

    Enum 관련 API 구현 (#13)

---
+++ src/main/java/isang/orangeplanet/domain/enum_api/controller/EnumController.java
@@ -0,0 +1,32 @@
+package isang.orangeplanet.domain.enum_api.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import isang.orangeplanet.domain.enum_api.controller.response.ListEnumResponse;
+import isang.orangeplanet.domain.enum_api.service.EnumService;
+import isang.orangeplanet.global.api_response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * EnumController : Enum 관련 Controller
+ */
+@RestController
+@RequestMapping(value = "/enum")
+@RequiredArgsConstructor
+@Tag(name = "Enum(선택형 필드)", description = "Enum 관련 API")
+public class EnumController {
+  private final EnumService enumService;
+
+  /**
+   * 뱃지 목록 조회
+   * @return : 뱃지 Enum 목록 반환
+   */
+  @GetMapping(value = "/badge")
+  @Operation(summary = "뱃지", description = "뱃지 Enum 목록 조회")
+  public ApiResponse<ListEnumResponse> getBadgeEnumList() {
+    return ApiResponse.onSuccess(enumService.getBadgeEnumList());
+  }
+}
---
+++ src/main/java/isang/orangeplanet/domain/enum_api/controller/response/ListEnumResponse.java
@@ -0,0 +1,23 @@
+package isang.orangeplanet.domain.enum_api.controller.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Schema(description = "Enum 조회 응답 객체")
+public class ListEnumResponse {
+
+  @Schema(description = "Enum 목록")
+  private List<ListEnumDto> badgeList;
+}
