edda731c95d5940666746e09cdd49d97a6991ad5
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-08-08 19:31:40 +0900

    모든 코드 Import문 정리 (#35)

--- src/main/java/isang/orangeplanet/domain/test/controller/TestController.java
+++ src/main/java/isang/orangeplanet/domain/test/controller/TestController.java
@@ -1,5 +1,6 @@
 package isang.orangeplanet.domain.test.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import isang.orangeplanet.domain.test.controller.request.CreateTestRequest;
@@ -25,7 +26,7 @@ import org.springframework.web.bind.annotation.*;
  * ex) @RequestMapping("/user"), @Tag(name = "User", description = "회원 관련 API")
  */
 
-//@Hidden
+@Hidden
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
--- src/main/java/isang/orangeplanet/domain/user/controller/dto/UserSimpleDto.java
+++ src/main/java/isang/orangeplanet/domain/user/controller/dto/UserSimpleDto.java
@@ -1,10 +1,6 @@
 package isang.orangeplanet.domain.user.controller.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import isang.orangeplanet.global.utils.enums.Role;
 import lombok.*;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Builder
